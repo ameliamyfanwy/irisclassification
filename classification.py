1dfd33d47d08eda6656d40046e7b217deb647c8e
Amelia Lloyd <amelialloyd31@gmail.com>
2020-06-27 12:01:51 +0100

    Split data for training

--- classification.py
+++ classification.py
@@ -1,22 +1,32 @@
 import matplotlib.pyplot as plt 
 
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 iris = load_iris()
 
-features = iris.data.T
+# Split data set into features
+# features = iris.data.T
 
-sepal_length = features[0]
-sepal_width = features[1]
-petal_length = features[2]
-petal_width = features[3]
+# Assign features to variables
+# sepal_length = features[0]
+# sepal_width = features[1]
+# petal_length = features[2]
+# petal_width = features[3]
 
-sepal_length_label = iris.feature_names[0]
-sepal_width_label = iris.feature_names[1]
-petal_length_label = iris.feature_names[2]
-petal_width_label = iris.feature_names[3]
+# Assign names to variables for use when plotting
+# sepal_length_label = iris.feature_names[0]
+# sepal_width_label = iris.feature_names[1]
+# petal_length_label = iris.feature_names[2]
+# petal_width_label = iris.feature_names[3]
 
-plt.scatter(petal_length, petal_width, c=iris.target)
-plt.xlabel(petal_length_label)
-plt.ylabel(petal_width_label)
-plt.show()
+# Create plot
+# plt.scatter(petal_length, petal_width, c=iris.target)
+# plt.xlabel(petal_length_label)
+# plt.ylabel(petal_width_label)
+# plt.show()
+
+# Splitting data for training using train_test_split
+X_train, X_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=0)
+
