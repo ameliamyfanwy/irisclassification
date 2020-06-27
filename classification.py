106fb133d22f5ad9d17a6810784413c9b280d9d1
Amelia Lloyd <amelialloyd31@gmail.com>
2020-06-27 11:26:49 +0100

    Split attributes and add labels

--- classification.py
+++ classification.py
@@ -1,1 +1,19 @@
+#import matplotlib.pylot as plt 
+
 from sklearn.datasets import load_iris
+
+iris = load_iris()
+
+features = iris.data.T
+
+sepal_length = features[0]
+sepal_width = features[1]
+petal_length = features[2]
+petal_width = features[3]
+
+sepal_length_label = iris.feature_names[0]
+sepal_width_label = iris.feature_names[1]
+petal_length_label = iris.feature_names[2]
+petal_width_label = iris.feature_names[3]
+
+print(petal_width_label)
